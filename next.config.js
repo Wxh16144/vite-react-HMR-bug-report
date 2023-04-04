7eb04cf4f8b31182be68dcd50eb382cc709cda8b
wuxh <wxh1220@gmail.com>
2023-04-04 10:26:54 +0800

    chore: 支持 less

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withAntdLess = require('next-plugin-antd-less');  
+
 const nextConfig = {
   reactStrictMode: true,
+  ...withAntdLess({}),
 }
 
 module.exports = nextConfig
